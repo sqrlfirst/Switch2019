8cc398fa67a7912f38f02ec5363b35439e8308b3
sqrlfirst <44490789+sqrlfirst@users.noreply.github.com>
2019-05-08 12:20:18 +0700

    Add SA port to frame receiver

--- Receiver/Ethernet_rx_frame.sv
+++ Receiver/Ethernet_rx_frame.sv
@@ -4,13 +4,15 @@ module Ethernet_rx_frame
         input wire          i_rx_dv,
         input wire          i_rx_er,
         input wire  [7:0]   i_rx_d,
-    
+        input wire          ishowSA,
         output wire [2:0]   o_fsm_state,
-        output reg         o_fsm_state_changed = 0,
+        output reg          o_fsm_state_changed = 0,
     
         output wire         o_rx_dv_4cd,
         output wire         o_rx_er_4cd,
-        output wire [7:0]   o_rx_d4cd
+        output wire [7:0]   o_rx_d4cd,
+        output wire [13:0]  osa,
+        output reg          onewsa
     );
     
     localparam [2:0] lpND  =  3'b000,     // No data
@@ -29,8 +31,10 @@ module Ethernet_rx_frame
     reg [4:0]       r_rx_dv_buffer = 5'b0;
     reg [10:0]      counter = 11'd0;
     int             i = 0;
-	reg toggle = 1'b0;
-	
+    reg toggle = 1'b0;
+    
+    reg [13:0]      rSA = 14'b0;
+
     // Sequential logic
     always @(posedge i_rx_clk) begin 
         if (i_rx_dv && !i_rx_er) 
@@ -78,13 +82,14 @@ module Ethernet_rx_frame
                 end
                 (lpSA): begin           
                     if (o_fsm_state_changed == 1) begin
-                        o_fsm_state_changed <=0;
-                        counter <= counter - 11'b1;
-                    end    
+                        o_fsm_state_changed <=0;                                           //   BELOW PLACE FOR CHANGE SMTH IF TEST IS NOT OKAY
+                        counter <= counter - 11'b1;                                        //   BELOW PLACE FOR CHANGE SMTH IF TEST IS NOT OKAY
+                    end 
                     else if (counter == 11'd0) begin
                         r_state_reg <= r_state_next;
                         o_fsm_state_changed <= 1;
                         counter <= 11'd1500;
+                        rSA <= {r_data_buffer[3][5:0],r_data_buffer[4]} ;                  // add other 8 bytes to reg
                     end    
                     else counter <= counter - 11'b1;
                 end
@@ -165,63 +170,78 @@ module Ethernet_rx_frame
         assign  o_fsm_state = r_state_reg;
         
         
-function integer eth_crc32_8d(input integer crc32, input logic [7:0] data);
-	bit [ 7:0] d;
-    bit [31:0] c;
-    bit [31:0] newcrc;
-    begin
-	    for (int i=0;i<8;i++) d[i] = data[7-i];
-        
-        c = crc32;
-        newcrc[0] = d[6] ^ d[0] ^ c[24] ^ c[30];
-        newcrc[1] = d[7] ^ d[6] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[30] ^ c[31];
-        newcrc[2] = d[7] ^ d[6] ^ d[2] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[26] ^ c[30] ^ c[31];
-        newcrc[3] = d[7] ^ d[3] ^ d[2] ^ d[1] ^ c[25] ^ c[26] ^ c[27] ^ c[31];
-        newcrc[4] = d[6] ^ d[4] ^ d[3] ^ d[2] ^ d[0] ^ c[24] ^ c[26] ^ c[27] ^ c[28] ^ c[30];
-        newcrc[5] = d[7] ^ d[6] ^ d[5] ^ d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[27] ^ c[28] ^ c[29] ^ c[30] ^ c[31];
-        newcrc[6] = d[7] ^ d[6] ^ d[5] ^ d[4] ^ d[2] ^ d[1] ^ c[25] ^ c[26] ^ c[28] ^ c[29] ^ c[30] ^ c[31];
-        newcrc[7] = d[7] ^ d[5] ^ d[3] ^ d[2] ^ d[0] ^ c[24] ^ c[26] ^ c[27] ^ c[29] ^ c[31];
-        newcrc[8] = d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[0] ^ c[24] ^ c[25] ^ c[27] ^ c[28];
-        newcrc[9] = d[5] ^ d[4] ^ d[2] ^ d[1] ^ c[1] ^ c[25] ^ c[26] ^ c[28] ^ c[29];
-        newcrc[10] = d[5] ^ d[3] ^ d[2] ^ d[0] ^ c[2] ^ c[24] ^ c[26] ^ c[27] ^ c[29];
-        newcrc[11] = d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[3] ^ c[24] ^ c[25] ^ c[27] ^ c[28];
-        newcrc[12] = d[6] ^ d[5] ^ d[4] ^ d[2] ^ d[1] ^ d[0] ^ c[4] ^ c[24] ^ c[25] ^ c[26] ^ c[28] ^ c[29] ^ c[30];
-        newcrc[13] = d[7] ^ d[6] ^ d[5] ^ d[3] ^ d[2] ^ d[1] ^ c[5] ^ c[25] ^ c[26] ^ c[27] ^ c[29] ^ c[30] ^ c[31];
-        newcrc[14] = d[7] ^ d[6] ^ d[4] ^ d[3] ^ d[2] ^ c[6] ^ c[26] ^ c[27] ^ c[28] ^ c[30] ^ c[31];
-        newcrc[15] = d[7] ^ d[5] ^ d[4] ^ d[3] ^ c[7] ^ c[27] ^ c[28] ^ c[29] ^ c[31];
-        newcrc[16] = d[5] ^ d[4] ^ d[0] ^ c[8] ^ c[24] ^ c[28] ^ c[29];
-        newcrc[17] = d[6] ^ d[5] ^ d[1] ^ c[9] ^ c[25] ^ c[29] ^ c[30];
-        newcrc[18] = d[7] ^ d[6] ^ d[2] ^ c[10] ^ c[26] ^ c[30] ^ c[31];
-        newcrc[19] = d[7] ^ d[3] ^ c[11] ^ c[27] ^ c[31];
-        newcrc[20] = d[4] ^ c[12] ^ c[28];
-        newcrc[21] = d[5] ^ c[13] ^ c[29];
-        newcrc[22] = d[0] ^ c[14] ^ c[24];
-        newcrc[23] = d[6] ^ d[1] ^ d[0] ^ c[15] ^ c[24] ^ c[25] ^ c[30];
-        newcrc[24] = d[7] ^ d[2] ^ d[1] ^ c[16] ^ c[25] ^ c[26] ^ c[31];
-        newcrc[25] = d[3] ^ d[2] ^ c[17] ^ c[26] ^ c[27];
-        newcrc[26] = d[6] ^ d[4] ^ d[3] ^ d[0] ^ c[18] ^ c[24] ^ c[27] ^ c[28] ^ c[30];
-        newcrc[27] = d[7] ^ d[5] ^ d[4] ^ d[1] ^ c[19] ^ c[25] ^ c[28] ^ c[29] ^ c[31];
-        newcrc[28] = d[6] ^ d[5] ^ d[2] ^ c[20] ^ c[26] ^ c[29] ^ c[30];
-        newcrc[29] = d[7] ^ d[6] ^ d[3] ^ c[21] ^ c[27] ^ c[30] ^ c[31];
-        newcrc[30] = d[7] ^ d[4] ^ c[22] ^ c[28] ^ c[31];
-        newcrc[31] = d[5] ^ c[23] ^ c[29];
-        eth_crc32_8d = newcrc;
+    function integer eth_crc32_8d(input integer crc32, input logic [7:0] data);
+    	bit [ 7:0] d;
+        bit [31:0] c;
+        bit [31:0] newcrc;
+        begin
+    	    for (int i=0;i<8;i++) d[i] = data[7-i];
+
+            c = crc32;
+            newcrc[0] = d[6] ^ d[0] ^ c[24] ^ c[30];
+            newcrc[1] = d[7] ^ d[6] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[30] ^ c[31];
+            newcrc[2] = d[7] ^ d[6] ^ d[2] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[26] ^ c[30] ^ c[31];
+            newcrc[3] = d[7] ^ d[3] ^ d[2] ^ d[1] ^ c[25] ^ c[26] ^ c[27] ^ c[31];
+            newcrc[4] = d[6] ^ d[4] ^ d[3] ^ d[2] ^ d[0] ^ c[24] ^ c[26] ^ c[27] ^ c[28] ^ c[30];
+            newcrc[5] = d[7] ^ d[6] ^ d[5] ^ d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[27] ^ c[28] ^ c[29] ^ c[30] ^ c[31];
+            newcrc[6] = d[7] ^ d[6] ^ d[5] ^ d[4] ^ d[2] ^ d[1] ^ c[25] ^ c[26] ^ c[28] ^ c[29] ^ c[30] ^ c[31];
+            newcrc[7] = d[7] ^ d[5] ^ d[3] ^ d[2] ^ d[0] ^ c[24] ^ c[26] ^ c[27] ^ c[29] ^ c[31];
+            newcrc[8] = d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[0] ^ c[24] ^ c[25] ^ c[27] ^ c[28];
+            newcrc[9] = d[5] ^ d[4] ^ d[2] ^ d[1] ^ c[1] ^ c[25] ^ c[26] ^ c[28] ^ c[29];
+            newcrc[10] = d[5] ^ d[3] ^ d[2] ^ d[0] ^ c[2] ^ c[24] ^ c[26] ^ c[27] ^ c[29];
+            newcrc[11] = d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[3] ^ c[24] ^ c[25] ^ c[27] ^ c[28];
+            newcrc[12] = d[6] ^ d[5] ^ d[4] ^ d[2] ^ d[1] ^ d[0] ^ c[4] ^ c[24] ^ c[25] ^ c[26] ^ c[28] ^ c[29] ^ c[30];
+            newcrc[13] = d[7] ^ d[6] ^ d[5] ^ d[3] ^ d[2] ^ d[1] ^ c[5] ^ c[25] ^ c[26] ^ c[27] ^ c[29] ^ c[30] ^ c[31];
+            newcrc[14] = d[7] ^ d[6] ^ d[4] ^ d[3] ^ d[2] ^ c[6] ^ c[26] ^ c[27] ^ c[28] ^ c[30] ^ c[31];
+            newcrc[15] = d[7] ^ d[5] ^ d[4] ^ d[3] ^ c[7] ^ c[27] ^ c[28] ^ c[29] ^ c[31];
+            newcrc[16] = d[5] ^ d[4] ^ d[0] ^ c[8] ^ c[24] ^ c[28] ^ c[29];
+            newcrc[17] = d[6] ^ d[5] ^ d[1] ^ c[9] ^ c[25] ^ c[29] ^ c[30];
+            newcrc[18] = d[7] ^ d[6] ^ d[2] ^ c[10] ^ c[26] ^ c[30] ^ c[31];
+            newcrc[19] = d[7] ^ d[3] ^ c[11] ^ c[27] ^ c[31];
+            newcrc[20] = d[4] ^ c[12] ^ c[28];
+            newcrc[21] = d[5] ^ c[13] ^ c[29];
+            newcrc[22] = d[0] ^ c[14] ^ c[24];
+            newcrc[23] = d[6] ^ d[1] ^ d[0] ^ c[15] ^ c[24] ^ c[25] ^ c[30];
+            newcrc[24] = d[7] ^ d[2] ^ d[1] ^ c[16] ^ c[25] ^ c[26] ^ c[31];
+            newcrc[25] = d[3] ^ d[2] ^ c[17] ^ c[26] ^ c[27];
+            newcrc[26] = d[6] ^ d[4] ^ d[3] ^ d[0] ^ c[18] ^ c[24] ^ c[27] ^ c[28] ^ c[30];
+            newcrc[27] = d[7] ^ d[5] ^ d[4] ^ d[1] ^ c[19] ^ c[25] ^ c[28] ^ c[29] ^ c[31];
+            newcrc[28] = d[6] ^ d[5] ^ d[2] ^ c[20] ^ c[26] ^ c[29] ^ c[30];
+            newcrc[29] = d[7] ^ d[6] ^ d[3] ^ c[21] ^ c[27] ^ c[30] ^ c[31];
+            newcrc[30] = d[7] ^ d[4] ^ c[22] ^ c[28] ^ c[31];
+            newcrc[31] = d[5] ^ c[23] ^ c[29];
+            eth_crc32_8d = newcrc;
+        end
+    endfunction
+
+    reg [31:0] rcrc_new = 'b0;
+
+    always @(posedge i_rx_clk) begin 
+        case(r_state_reg) 
+            lpPRE: toggle <= 1'b0;
+            lpSFD: rcrc_new <= '1;
+            lpDA, lpSA, lpDATA, lpCRC: rcrc_new <= eth_crc32_8d(rcrc_new, o_rx_d4cd);
+            lpND: begin 
+                if ((rcrc_new != 32'hC704DD7B )&(rcrc_new != 32'h0000_0000)) begin 
+                    toggle <= 1'b1;
+                end
+            end    
+        endcase    
     end
-endfunction
 
-reg [31:0] rcrc_new = 'b0;
+    // Showing SA to out                                                                //   BELOW PLACE FOR CHANGE SMTH IF TEST IS NOT OKAY
+    always @(posedge i_rx_clk) begin                                                    //   BELOW PLACE FOR CHANGE SMTH IF TEST IS NOT OKAY
+        if(ishowSA) begin
+            osa <= rSA;
+        end
+        else begin
+            osa <= 14'bx;
+        end
+    end
 
-always @(posedge i_rx_clk) begin 
-    case(r_state_reg) 
-        lpPRE: toggle <= 1'b0;
-        lpSFD: rcrc_new <= '1;
-        lpDA, lpSA, lpDATA, lpCRC: rcrc_new <= eth_crc32_8d(rcrc_new, o_rx_d4cd);
-        lpND: begin 
-            if ((rcrc_new != 32'hC704DD7B )&(rcrc_new != 32'h0000_0000)) begin 
-                toggle <= 1'b1;
-            end
-        end    
-    endcase    
-end
+    always @(rSA or osa) begin
+        if (osa == 14'bx) onewsa <= 1'b0;
+        else onewsa <= 1'b1;
+    end
 
 endmodule
